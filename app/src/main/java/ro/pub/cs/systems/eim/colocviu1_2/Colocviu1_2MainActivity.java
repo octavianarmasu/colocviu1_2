4da3e9da54799025dfa14a3b72e5fa69af5c35c2
armasuoctavian <octavian.armasu@gmail.com>
2025-11-10 08:33:45 +0200

    B.1 done

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.java
@@ -1,6 +1,9 @@
 package ro.pub.cs.systems.eim.colocviu1_2;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,52 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Colocviu1_2MainActivity extends AppCompatActivity {
 
+    private EditText nextTerms, allTerms;
+    private Button add, compute;
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String text = nextTerms.getText().toString().trim();
+            String nextText = allTerms.getText().toString();
+
+            if (view.getId() == R.id.add) {
+                if (isInteger(text)) {
+                    nextText += text + "+";
+                }
+                allTerms.setText(nextText);
+                nextTerms.setText("");
+            }
+
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test01_2_main);
 
+        nextTerms = findViewById(R.id.next_term);
+        allTerms = findViewById(R.id.all_terms);
+        add = findViewById(R.id.add);
+        compute = findViewById(R.id.compute);
+        add.setOnClickListener(buttonClickListener);
+
+
+    }
+
+    private boolean isInteger(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
